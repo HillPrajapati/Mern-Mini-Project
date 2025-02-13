3c727fd23fe747b57987be39bda1055ce9a69773
HillPrajapati <hillprajapati317@gmail.com>
2025-02-13 18:51:04 +0530

    Create appConstants.js

---
+++ Frontend/constant/appConstants.js
@@ -0,0 +1,20 @@
+// appConstants.js
+export const USER_MESSAGES = {
+    USER_REGISTERED: "User registered successfully",
+    INVALID_CREDENTIALS: "Invalid credentials",
+    LOGIN_SUCCESSFUL: "Login successful",
+    DEPARTMENT_CREATED: "Department created successfully",
+    DEPARTMENT_UPDATED: "Department updated successfully",
+    DEPARTMENT_DELETED: "Department deleted successfully",
+    DEPARTMENT_FETCH_ERROR:"Error while fetching Department details.",
+    USER_NOT_FOUND: "User not found",
+  };
+  
+  export const STATUS_CODES = {
+    SUCCESS: 200,
+    CREATED: 201,
+    UNAUTHORIZED: 401,
+    NOT_FOUND: 404,
+    INTERNAL_SERVER_ERROR: 500,
+  };
+  
